dce357541f719660b0c324cd223ce3f144acf348
Stephan Joneleit <stephan.joneleit@zeiss.com>
2022-01-04 10:45:24 +0100

    js type system is...???

--- toolcomparer/src/components/Tool/ToolCriteriumSuitabilityCard.vue
+++ toolcomparer/src/components/Tool/ToolCriteriumSuitabilityCard.vue
@@ -93,7 +93,7 @@
               :items="fullfillmentItems"
               label="Fullfillment"
               v-model="selectedFullfillment"
-              :rules="!$store.getters.getSettingsIsDescriptionMandatory || rules.str"
+              :rules="[(!$store.getters.getSettingsIsDescriptionMandatory || rules.str)]"
               :required="$store.getters.getSettingsIsDescriptionMandatory"
               :readonly="!isInCreation()"
               @change="updateFullfillment(selectedFullfillment)"
@@ -110,7 +110,7 @@
               outlined
               label="Justification"
               v-model="toolKVSuitabilityItem.suitability.justification"
-              :rules="rules.str || !$store.getters.getSettingsIsJustificationMandatory"
+              :rules="[(rules.str || !$store.getters.getSettingsIsJustificationMandatory)]"
               :required="$store.getters.getSettingsIsJustificationMandatory"
               :readonly="!isInCreation()"
               no-resize
